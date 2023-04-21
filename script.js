03c6801782810225b21d1e1e875706fdad01627d
Vladimir Davydov <v.g.davydoff@gmail.com>
2023-04-21 14:38:34 +0300

    Repair Edits 1

--- script.js
+++ script.js
@@ -1,84 +1,74 @@
 const swiper = new Swiper('.swiper', {
     spaceBetween: 16,
-        slidesOffsetBefore: 16,
-        slidesOffsetAfter: 16,
-        slidesPerView: 'auto',
-        grabCursor: true,
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true,
+    slidesOffsetBefore: 16,
+    slidesOffsetAfter: 16,
+    slidesPerView: 'auto',
+    grabCursor: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+    breakpoints: {
+        768: {
+            initialSlide: 0,
+            spaceBetween: 0,
+            enabled: false
+            }
         },
-        breakpoints: {
-            768: {
-                initialSlide: 0,
-                spaceBetween: 0,
-                enabled: false
-              }
-        },
-  });
+});
 
-let labels = document.querySelectorAll('.label-wrapper__item');
-let expandButton = document.querySelector('.expand');
-let collapseButton = document.querySelector('.collapse');
-let labelWrapper = document.querySelector('.label-wrapper');
-let repairMain = document.querySelector('.repair-main');
-console.log(expandButton);
+const labels = document.querySelectorAll('.label-wrapper__item');
+const expandButton = document.querySelector('.expand');
+const labelWrapper = document.querySelector('.label-wrapper');
+const repairMain = document.querySelector('.repair-main');
+const expandButtonValue = expandButton.textContent;
+const collapseButtonValue = "Скрыть";
 
 if (window.innerWidth >= 768 && window.innerWidth < 1120) {
-    for (i = 0; i < labels.length; i++) {
+    for (let i = 0; i < labels.length; i++) {
         if (i > 5) {
             labels[i].classList.add('label-wrapper__item_hidden');
         }
     }
-    collapseButton.classList.add('collapse_hidden');
 }
 
 if (window.innerWidth >= 1120) {
-    for (i = 0; i < labels.length; i++) {
+    for (let i = 0; i < labels.length; i++) {
         if (i > 7) {
             labels[i].classList.add('label-wrapper__item_hidden');
         }
     }
-    collapseButton.classList.add('collapse_hidden');
 }
 
+expandButton.addEventListener('click', function() {
 
-expandButton.addEventListener('click', function(env) {
-    env.preventDefault();
-
-    for (i = 0; i < labels.length; i++) {
-        labels[i].classList.remove('label-wrapper__item_hidden');
-    }
-    labelWrapper.classList.add('label-wrapper-medium');
-    repairMain.classList.add('repair-main_expanded');
-    expandButton.classList.add('expand_hidden');
-    expandButton.classList.remove('expand');
-    collapseButton.classList.add('collapse');
-    collapseButton.classList.remove('collapse_hidden');
-});
-
-collapseButton.addEventListener('click', function(env) {
-    env.preventDefault();
-
-    for (i = 0; i < labels.length; i++) {
-        if (window.innerWidth >= 1120) {
-            if (i > 7) {
-                labels[i].classList.add('label-wrapper__item_hidden');
-            }
-        } else {
-            if (i > 5) {
-                labels[i].classList.add('label-wrapper__item_hidden');
+    if (expandButton.textContent === expandButtonValue) {
+        for (let i = 0; i < labels.length; i++) {
+            labels[i].classList.remove('label-wrapper__item_hidden');
+        }
+        labelWrapper.classList.add('label-wrapper-medium');
+        repairMain.classList.add('repair-main_expanded');
+        expandButton.textContent = collapseButtonValue;
+        expandButton.classList.remove('expand');
+        expandButton.classList.add('collapse');
+    } else {
+        for (let i = 0; i < labels.length; i++) {
+            if (window.innerWidth >= 1120) {
+                if (i > 7) {
+                    labels[i].classList.add('label-wrapper__item_hidden');
+                }
+            } else {
+                if (i > 5) {
+                    labels[i].classList.add('label-wrapper__item_hidden');
+                }
             }
         }
+        
+        labelWrapper.classList.remove('label-wrapper_expanded');
+        repairMain.classList.remove('repair-main_expanded');
+        expandButton.textContent = "Показать все";
+        expandButton.classList.remove('collapse');
+        expandButton.classList.add('expand');
     }
-    
-    labelWrapper.classList.remove('label-wrapper_expanded');
-    repairMain.classList.remove('repair-main_expanded');
-    expandButton.classList.remove('expand_hidden');
-    expandButton.classList.add('expand');
-    collapseButton.classList.remove('collapse');
-    collapseButton.classList.add('collapse_hidden');
 });
-
-
