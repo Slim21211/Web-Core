71caebddb4750f1417a1b2253c7a59116c2b367b
Vladimir Davydov <v.g.davydoff@gmail.com>
2023-05-03 10:22:04 +0300

    empty area

--- js/modals-script.js
+++ js/modals-script.js
@@ -3,14 +3,18 @@ const mobileMenu = document.querySelector('.site-wrapper__item:nth-child(1)');
 const mainBlock = document.querySelector('.site-wrapper__item:nth-child(2)');
 const closeMenu = document.querySelector('.menu-close');
 
-console.log(mainBlock);
-
-menuIcon.addEventListener('click', function() {
-    mobileMenu.classList.remove('site-wrapper__item_hidden');
-    mainBlock.classList.add('site-wrapper__item_hidden');
+document.addEventListener('click', function(event) {
+    if (event.target === menuIcon) {
+        mobileMenu.classList.remove('site-wrapper__item_hidden');
+        mainBlock.classList.add('site-wrapper__item_hidden');
+    }
+    if (event.target === closeMenu) {
+        mobileMenu.classList.add('site-wrapper__item_hidden');
+        mainBlock.classList.remove('site-wrapper__item_hidden');
+    }
+    if (!event.target.closest('.mobile-menu') && event.target != menuIcon) {
+        mobileMenu.classList.add('site-wrapper__item_hidden');
+        mainBlock.classList.remove('site-wrapper__item_hidden');
+    }
 })
 
-closeMenu.addEventListener('click', function() {
-    mobileMenu.classList.add('site-wrapper__item_hidden');
-    mainBlock.classList.remove('site-wrapper__item_hidden');
-})
