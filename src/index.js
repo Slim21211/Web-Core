b1a50a17ee4826d831a5d9f4bc977abeaeb129c1
Vladimir Davydov <v.g.davydoff@gmail.com>
2023-05-04 12:16:37 +0300

    pack v.1

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './index.html';
+import './index.scss';
+
+let script = require('../src/js/script');
+let modals = require('../src/js/modals-script');
+let prices = require('../src/js/prices-script');
+let gadgets = require('../src/js/gadgets-script');
+let repair = require('../src/js/repair-script.js');
